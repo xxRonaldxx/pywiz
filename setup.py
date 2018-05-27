198736bd691ae03116db1e48d77fecb77bb6c786
Ron Gatewood <ronald.l.gatewood@gmail.com>
2018-05-26 23:41:04 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding="utf8") as fh:
 
 setuptools.setup(
     name="pywiz",
-    version="0.0.2",
+    version="0.0.3",
     author="Ron Gatewood",
     author_email="ronald.l.gatewood@gmail.com",
     description="A PyQt5 wizard package",
